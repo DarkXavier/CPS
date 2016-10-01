8a6aeb2aec48c6f884056245eb1273b0994e6550
Christian Adan Israel <camezcua@mimoni.com>
2016-10-01 15:51:47 -0500

    Se hicieron correcciones generales de la bienvenida.

--- src/app/config.route.js
+++ src/app/config.route.js
@@ -37,8 +37,8 @@
 
 
         // set default routes when no path specified
-        $urlRouterProvider.when('', '/carnet');
-        $urlRouterProvider.when('/', '/carnet');
+        $urlRouterProvider.when('', '/login');
+        $urlRouterProvider.when('/', '/login');
 
         // always goto 404 if route not found
         $urlRouterProvider.otherwise('/404');
